8f4956b6075def5965d960fa65b8a79ef323496d
Ričardas Pocius <cirka@cirka.lt>
2018-05-03 11:48:34 +0300

    Make it compile

--- do.sh
+++ do.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cc=$(which clang-3.6||which gcc-4.9||which clang||||which gcc)
+cc=$(which clang-3.6||which gcc-4.9||which clang||which gcc)
 so=$(test -f /etc/asl.conf && printf dylib|| printf so)
-$cc "-Dinline=__attribute__((__always_inline__))" -O3 -march=native -std=c11 -Wextra -Wpedantic -Wall -dynamic -shared keccak-tiny.c -o libkeccak-tiny.$so
-$cc -Os -march=native -std=c11 -Wextra -Wpedantic -Wall -dynamic -shared keccak-tiny.c -o libkeccak-tiny-small.$so
+$cc "-Dinline=__attribute__((__always_inline__))" -D"memset_s(W,WL,V,OL)=memset(W,V,OL)" -O3 -march=native -std=c11 -Wextra -Wpedantic -Wall -fPIC  -c keccak-tiny.c -o libkeccak-tiny.o
+#$cc -Os -march=native -std=c11 -Wextra -Wpedantic -Wall -fPIC -shared keccak-tiny.c -o libkeccak-tiny-small.$so
